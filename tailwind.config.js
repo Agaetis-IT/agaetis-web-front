69b90fc0e47bd0d686c2f4afe3f0613567c0959d
benoit munoz <mun.benoit@gmail.com>
2019-04-19 17:03:46 +0200

    :pencil2: Add Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,14 +67,10 @@ let colors = {
   'orange-darkest': '#462a16',
   'orange-darker': '#613b1f',
   'orange-dark': '#de751f',
-  'orange': '#f6993f',
+  'orange': '#ff7f40',
   'orange-light': '#faad63',
   'orange-lighter': '#fcd9b6',
   'orange-lightest': '#fff5eb',
-
-  'agaetis': '#FF7F40',
-
-
 
   'yellow-darkest': '#453411',
   'yellow-darker': '#684f1d',
