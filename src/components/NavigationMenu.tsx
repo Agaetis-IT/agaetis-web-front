4d9e86fd9a23460fee2a757131d58c5624bb4ddf
benoit munoz <mun.benoit@gmail.com>
2019-04-23 15:47:43 +0200

    :lipstick: Update general style

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -79,19 +79,22 @@ export default function NavigationMenu() {
           </Button>
         ))}
       </div>
-      <div className="hidden md:inline md:ml-16">
-        <Button href="#" className="px-6 py-3 leading-none rounded-full uppercase mt-4 md:mt-0 text-white bg-orange">
+      <div className="hidden md:inline md:ml-12">
+        <Button
+          href="#"
+          className="px-6 py-3 leading-none rounded-full uppercase mt-4 md:mt-0 text-white bg-orange text-xs font-semibold"
+        >
           Contact
         </Button>
       </div>
       <div className="inline md:hidden text-xs font-medium md:flex-grow">
-        <Button href="#" className="md:mt-0 md:mr-16 md:ml-1 p-2 md:p-0 text-white">
+        <Button href="#" className="md:mt-0 md:mr-16 md:ml-1 p-2 md:p-0 text-white text-xs font-semibold">
           Contact
         </Button>
       </div>
       {/*Languages selection */}
       <div className="text-xs md:ml-4 flex flex-row items-center p-2 md:p-0 text-white">
-        <Button href="#" className="mr-1 text-white">
+        <Button href="#" className="mr-1 text-white text-xss">
           EN
         </Button>
         <span className="text-white mr-1 hidden md:inline align-middle leading-none"> - </span>
