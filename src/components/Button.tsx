a034946a9c7f904183b90da0c7ca1877ed3ef8c7
benoit munoz <mun.benoit@gmail.com>
2019-04-23 16:35:57 +0200

    :ok_hand: changes from code review

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,33 +2,17 @@ import clsx from 'clsx'
 import Link from 'next/link'
 import React from 'react'
 
-import { Color } from './type'
-
 interface Props {
   href?: string
-  color?: Color
   children: string
   className?: string
   onClick?(e: React.MouseEvent): void
 }
 
-function getBgClass(colorProp?: Color): string {
-  switch (colorProp) {
-    case 'orange':
-      return 'bg-orange'
-    default:
-      return ''
-  }
-}
-
-export default function Button({ href, color, children, onClick, className }: Props) {
+export default function Button({ href, children, onClick, className }: Props) {
   const Component = !href ? 'button' : 'a'
   const baseComponent = (
-    <Component
-      href={href}
-      onClick={onClick}
-      className={clsx(className, getBgClass(color), 'block md:inline-block text-xs text-white')}
-    >
+    <Component href={href} onClick={onClick} className={clsx(className, 'block md:inline-block')}>
       {children}
     </Component>
   )
