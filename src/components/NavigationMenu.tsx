fadff0691c6922e5a4388953e53bd9c52114dad2
benoit munoz <mun.benoit@gmail.com>
2019-04-25 14:42:49 +0200

    :twisted_rightwards_arrows: Rebase i18n

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -29,7 +29,6 @@ export default function NavigationMenu() {
     <div className="block bg-orange md:bg-transparent flex-grow md:flex-no-grow md:flex md:items-center md:w-auto p-4 md:p-0">
       <div className="text-xs font-medium md:flex-grow">
         {pages.map(page => (
-<<<<<<< i18n
           <Link key={page} href="#">
             <Button href="#responsive-header" className="p-2 md:p-3 text-white text-xs font-semibold">
               {page}
@@ -73,34 +72,6 @@ export default function NavigationMenu() {
           className={clsx({ 'Button-language--active': isLanguageSelected('fr') }, 'text-black md:text-white text-xss')}
           onClick={onLanguageChange('fr')}
         >
-=======
-          <Button key={page} href="#responsive-header" className="p-2 md:p-3 text-white">
-            {page}
-          </Button>
-        ))}
-      </div>
-      <div className="hidden md:inline md:ml-12">
-        <Button
-          href="#"
-          className="px-6 py-3 leading-none rounded-full uppercase mt-4 md:mt-0 text-white bg-orange text-xs font-semibold"
-        >
-          Contact
-        </Button>
-      </div>
-      <div className="inline md:hidden text-xs font-medium md:flex-grow">
-        <Button href="#" className="md:mt-0 md:mr-16 md:ml-1 p-2 md:p-0 text-white text-xs font-semibold">
-          Contact
-        </Button>
-      </div>
-      {/*Languages selection */}
-      <div className="text-xs md:ml-4 flex flex-row items-center p-2 md:p-0 text-white">
-        <Button href="#" className="mr-1 text-white text-xss">
-          EN
-        </Button>
-        <span className="text-white mr-1 hidden md:inline align-middle leading-none"> - </span>
-        <span className="text-white mr-1 block md:hidden align-middle leading-none">|</span>
-        <Button href="#" className="text-white">
->>>>>>> :sparkles: Add Footer menu
           FR
         </Button>
       </div>
