d6c878db0bf309eac059254ed3e6bc90e360afd1
benoit munoz <mun.benoit@gmail.com>
2019-04-25 14:42:49 +0200

    :twisted_rightwards_arrows: Rebase i18n

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,11 @@ import React from 'react'
 
 interface Props {
   href?: string
-<<<<<<< i18n
-  children: string
   component?: React.ReactType
-=======
   children?: string | React.ReactElement
->>>>>>> :sparkles: Add Footer menu
   className?: string
   onClick?(e: React.MouseEvent): void
 }
-
-<<<<<<< i18n
 export default function Button({ href, children, component, onClick, className }: Props) {
   let ComponentProp
   if (component) {
@@ -26,12 +20,6 @@ export default function Button({ href, children, component, onClick, className }
 
   return (
     <ComponentProp href={href} onClick={onClick} className={clsx(className, 'block md:inline-block cursor-pointer')}>
-=======
-export default function Button({ href, children, onClick, className }: Props) {
-  const Component = !href ? 'button' : 'a'
-  const baseComponent = (
-    <Component href={href} onClick={onClick} className={clsx('block md:inline-block text-xs', className)}>
->>>>>>> :sparkles: Add Footer menu
       {children}
     </ComponentProp>
   )
