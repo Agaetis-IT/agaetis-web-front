56ac4f2776051b369dc5c9a140187903b0872499
benoit munoz <mun.benoit@gmail.com>
2019-04-26 14:40:53 +0200

    :lipstick: Fix Header/footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
       </div>
       <hr className="md:mt-8 Footer-separator" />
       <BottomNav />
-      <hr />
+      <hr className="Footer-separator" />
       <div className="text-xss text-center opacity-25 py-2"> {t('footer.copyright')}</div>
     </footer>
   )
