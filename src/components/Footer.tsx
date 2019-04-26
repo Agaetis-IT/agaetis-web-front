aa46352dfcd3fc336640fb7f5122ba7cec81360f
benoit munoz <mun.benoit@gmail.com>
2019-04-26 11:38:28 +0200

    :art: change translation architecture

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,19 +10,36 @@ import BottomNav from './BottomNav'
 import Button from './Button'
 import './Footer.css'
 
-const addresses = [
-  { agency: 'Clermont', address: '9, allée Evariste Galois', zipcode: '63170', city: 'Aubière', tel: '04 73 35 47 51' },
-  { agency: 'Paris', address: '21, rue de la banque', zipcode: '75002', city: 'Paris', tel: '01 44 63 53 13' },
-  { agency: 'Lyon', address: '52, Quai Rambaud', zipcode: '69002', city: 'Lyon', tel: '' },
-]
-
 export default function Footer() {
   const { t } = useTranslation()
+  const addresses = [
+    {
+      agency: t('footer.clermont'),
+      address: t('footer.clermont-address'),
+      zipcode: t('footer.clermont-zipcode'),
+      city: t('footer.clermont-city'),
+      tel: t('footer.clermont-tel'),
+    },
+    {
+      agency: t('footer.paris'),
+      address: t('footer.paris-address'),
+      zipcode: t('footer.paris-zipcode'),
+      city: t('footer.paris-city'),
+      tel: t('footer.paris-tel'),
+    },
+    {
+      agency: t('footer.lyon'),
+      address: t('footer.lyon-address'),
+      zipcode: t('footer.lyon-zipcode'),
+      city: t('footer.lyon-city'),
+      tel: t('footer.lyon-tel'),
+    },
+  ]
   return (
     <footer className="p-4">
       <div className="flex flex-col md:flex-row justify-center text-center md:text-left">
         <div className="p-4 md:pl-0">
-          <h2 className="text-xs">{t('footer.addresses.title')}</h2>
+          <h2 className="text-xs">{t('footer.address-title')}</h2>
           <div className="flex flex-col md:flex-row mt-4 justify-center font-thin">
             {addresses.map(address => (
               <Address key={address.agency} {...address} />
@@ -30,7 +47,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="p-4 py-0 md:py-4 md:px-0">
-          <h2 className="text-xs">{t('footer.social.title')}</h2>
+          <h2 className="text-xs">{t('footer.social-title')}</h2>
           <div className="flex flex-row my-4 justify-center md:justify-start">
             <Button href="#" className="mr-4 w-4">
               <img src={Facebook} />
@@ -47,7 +64,7 @@ export default function Footer() {
       <hr className="md:mt-8 Footer-separator" />
       <BottomNav />
       <hr />
-      <div className="text-xss text-center opacity-25 py-2">&copy;AGAETIS - {t('footer.copyright')}</div>
+      <div className="text-xss text-center opacity-25 py-2"> {t('footer.copyright')}</div>
     </footer>
   )
 }
