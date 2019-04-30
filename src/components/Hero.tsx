d79cea07c1de362c298e73c43145f14738605954
benoit munoz <mun.benoit@gmail.com>
2019-04-30 10:21:59 +0200

    :lipstick: Fix mobile navigation display

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <div className="hero m-0">
       <div className="darkener flex justify-center">
-        <div className="p-6 md:p-8 md:my-6 py-16 md:py-32 max-w-sm md:max-w-md text-white mx-auto pr-12 md:pr-40 text-justify">
+        <div className="p-6 md:p-10 md:my-6 py-16 md:py-32 max-w-sm md:max-w-md text-white mx-auto pr-12 md:pr-40 text-justify">
           <h1>
             {t('index.explore')}
             <br />
