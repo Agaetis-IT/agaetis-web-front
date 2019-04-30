48c4c50aef5fae9c023c15f7ef1783e11822fb5a
benoit munoz <mun.benoit@gmail.com>
2019-04-30 15:42:03 +0200

    :lipstick: general style update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,15 +7,15 @@ export default function Hero() {
   return (
     <div className="hero m-0">
       <div className="darkener flex justify-center">
-        <div className="p-6 md:p-10 md:my-6 py-16 md:py-32 max-w-sm md:max-w-md text-white mx-auto pr-12 md:pr-40 text-justify">
-          <h1>
+        <div className="p-6 md:p-10 md:my-6 py-16 md:py-32 max-w-sm md:max-w-md text-white mx-auto md:pr-40 text-justify justify-fix">
+          <h1 className="md:text-4xl">
             {t('index.explore')}
             <br />
             {t('index.valorize')}
             <br />
             {t('index.innovate')}
           </h1>
-          <p className=" text-xs md:pr-10 pt-2 leading-normal">{t('index.hero-description')}</p>
+          <p className="text-sm md:pr-10 pt-4 leading-normal">{t('index.hero-description')}</p>
         </div>
       </div>
     </div>
