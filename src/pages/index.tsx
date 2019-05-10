d018e3d4b9ac31c359231680b80915b66eab30bf
benoit munoz <mun.benoit@gmail.com>
2019-05-10 14:49:53 +0200

    :ok_hand: Create wpService

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,19 @@ import { useTranslation } from 'react-i18next'
 import Hero from '../components/Hero'
 import HomeCard from '../components/HomeCard'
 import Layout from '../components/Layout'
-import getIndexContent from '../Services/GetPageDescription'
+import { getIndexContent } from '../Services/wordpressService'
+import IndexContent from '../types/IndexContent'
 
 interface Props {
-  initialData: {
-    agaetis_desc_title: string
-    agaetis_desc: string
-    agaetis_desc_img: string
-    ideas_desc_title: string
-    ideas_desc: string
-    ideas_desc_img: string
-    solutions_desc_title: string
-    solutions_desc: string
-    solutions_desc_img: string
-    jobs_desc_title: string
-    jobs_desc: string
-    jobs_desc_img: string
-  }
+  pageContent: IndexContent
 }
 
 Index.getInitialProps = async () => {
-  const { acf: initialData } = await getIndexContent(13)
-  return { initialData }
+  const pageContent = await getIndexContent()
+  return { pageContent }
 }
 
-function Index({ initialData }: Props) {
+function Index({ pageContent: pageContent }: Props) {
   const { t } = useTranslation()
   return (
     <Layout headerProps={{ invertColors: true, className: 'header md:absolute md:mx-auto' }}>
@@ -37,39 +25,39 @@ function Index({ initialData }: Props) {
         <div className="px-2 md:px-6">
           <HomeCard
             className="md:flex-row"
-            title={initialData.agaetis_desc_title}
-            description={initialData.agaetis_desc}
+            title={pageContent.agaetis_desc_title}
+            description={pageContent.agaetis_desc}
             href="/agaetis"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pr-6"
-            imgUrl={initialData.agaetis_desc_img}
+            imgUrl={pageContent.agaetis_desc_img}
           />
           <HomeCard
             className="md:flex-row-reverse py-4 bg-grey"
-            title={initialData.ideas_desc_title}
-            description={initialData.ideas_desc}
+            title={pageContent.ideas_desc_title}
+            description={pageContent.ideas_desc}
             href="/ideas"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pl-6"
-            imgUrl={initialData.ideas_desc_img}
+            imgUrl={pageContent.ideas_desc_img}
           />
           <HomeCard
             className="md:flex-row"
-            title={initialData.solutions_desc_title}
-            description={initialData.solutions_desc}
+            title={pageContent.solutions_desc_title}
+            description={pageContent.solutions_desc}
             href="/solutions"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pr-6"
-            imgUrl={initialData.solutions_desc_img}
+            imgUrl={pageContent.solutions_desc_img}
           />
           <HomeCard
             className="md:flex-row-reverse py-4 bg-grey"
-            title={initialData.jobs_desc_title}
-            description={initialData.jobs_desc}
+            title={pageContent.jobs_desc_title}
+            description={pageContent.jobs_desc}
             href="/jobs"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pl-6"
-            imgUrl={initialData.jobs_desc_img}
+            imgUrl={pageContent.jobs_desc_img}
           />
         </div>
       </>
