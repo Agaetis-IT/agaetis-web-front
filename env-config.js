382cc2e81a4e69e744118084a042ee8427008611
benoit munoz <mun.benoit@gmail.com>
2019-05-10 10:30:20 +0200

    :ok_hand: Import all env vars

--- env-config.js
+++ env-config.js
@@ -1,4 +1,4 @@
 const { parsed: dotEnvResult } = require('dotenv').config()
 module.exports = {
-  'process.env.BACKEND_URL': dotEnvResult.BASE_URL,
+  dotEnvResult,
 }
--- src/Services/GetPageDescription.tsx
+++ src/Services/GetPageDescription.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default async function getIndexContent(id: number) {
-  const { data } = await axios.get(process.env.BACKEND_URL + 'wp-json/wp/v2/pages/' + id)
+  const { data } = await axios.get(process.env.BASE_URL + 'wp-json/wp/v2/pages/' + id)
   return data
 }
