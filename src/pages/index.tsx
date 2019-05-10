a972894ee8b315dc6891f106e393ebc00c2aa112
benoit munoz <mun.benoit@gmail.com>
2019-05-10 10:23:48 +0200

    :ok_hand: Add .env + indexProps interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,7 @@ import Layout from '../components/Layout'
 import getIndexContent from '../Services/GetPageDescription'
 
 interface Props {
-  initialData: Content
-}
-
-interface Content {
-  acf: {
+  initialData: {
     agaetis_desc_title: string
     agaetis_desc: string
     agaetis_desc_img: string
@@ -28,7 +24,7 @@ interface Content {
 }
 
 Index.getInitialProps = async () => {
-  const initialData = await getIndexContent(13)
+  const { acf: initialData } = await getIndexContent(13)
   return { initialData }
 }
 
@@ -41,39 +37,39 @@ function Index({ initialData }: Props) {
         <div className="px-2 md:px-6">
           <HomeCard
             className="md:flex-row"
-            title={initialData.acf.agaetis_desc_title}
-            description={initialData.acf.agaetis_desc}
+            title={initialData.agaetis_desc_title}
+            description={initialData.agaetis_desc}
             href="/agaetis"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pr-6"
-            imgUrl={initialData.acf.agaetis_desc_img}
+            imgUrl={initialData.agaetis_desc_img}
           />
           <HomeCard
             className="md:flex-row-reverse py-4 bg-grey"
-            title={initialData.acf.ideas_desc_title}
-            description={initialData.acf.ideas_desc}
+            title={initialData.ideas_desc_title}
+            description={initialData.ideas_desc}
             href="/ideas"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pl-6"
-            imgUrl={initialData.acf.ideas_desc_img}
+            imgUrl={initialData.ideas_desc_img}
           />
           <HomeCard
             className="md:flex-row"
-            title={initialData.acf.solutions_desc_title}
-            description={initialData.acf.solutions_desc}
+            title={initialData.solutions_desc_title}
+            description={initialData.solutions_desc}
             href="/solutions"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pr-6"
-            imgUrl={initialData.acf.solutions_desc_img}
+            imgUrl={initialData.solutions_desc_img}
           />
           <HomeCard
             className="md:flex-row-reverse py-4 bg-grey"
-            title={initialData.acf.jobs_desc_title}
-            description={initialData.acf.jobs_desc}
+            title={initialData.jobs_desc_title}
+            description={initialData.jobs_desc}
             href="/jobs"
             buttonContent={t('index.learnmore-btn')}
             descBlockClass="md:pl-6"
-            imgUrl={initialData.acf.jobs_desc_img}
+            imgUrl={initialData.jobs_desc_img}
           />
         </div>
       </>
