01203ef0531cdc321bb91f4ef88a6df96e65925a
benoit munoz <mun.benoit@gmail.com>
2019-05-10 14:49:53 +0200

    :ok_hand: Create wpService

--- src/Services/GetPageDescription.tsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export default async function getIndexContent(id: number) {
-  const { data } = await axios.get(process.env.BASE_URL + 'wp-json/wp/v2/pages/' + id)
-  return data
-}
