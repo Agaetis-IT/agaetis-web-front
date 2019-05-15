005cef05f2c729ecd3596f01643daf40be737300
benoit munoz <mun.benoit@gmail.com>
2019-05-15 12:07:38 +0200

    :art: Apply Periscope' SEO tips

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -6,8 +6,8 @@ import './Common.css'
 
 export default function ContactSection() {
   return (
-    <div className="bg-orange p-8 pt-12 md:pb-14 md:pt-20 flex flex-col md:flex-row justify-center text-white text-center md:text-left blue-underline mb-4">
-      <h2 className="text-2xl">Contact</h2>
+    <div className="bg-orange p-8 pt-12 md:pb-14 md:pt-16 flex flex-col md:flex-row justify-center text-white text-center md:text-left blue-underline mb-4">
+      <h3 className="text-2xl self-center m-0 md:pb-2">Contact</h3>
       <div className="md:ml-10 mt-8 md:mt-0">
         <p className="max-w-xs text-xs leading-normal">
           Pour toute demande de renseignements, n'hésitez pas à nous contacter, nous vous répondrons dans les plus brefs
--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -15,22 +15,34 @@ interface Props {
   imgUrl: string
 }
 
-export default function HomeCard({ className, title, description, buttonContent, descBlockClass, imgUrl }: Props) {
+export default function HomeCard({
+  className,
+  href,
+  title,
+  description,
+  buttonContent,
+  descBlockClass,
+  imgUrl,
+}: Props) {
   return (
     <div className={clsx('flex flex-col my-6 md:my-12 justify-between md:p-8 mx-auto md:max-w-md', className)}>
       <div className={clsx('md:max-w-xs mx-4 md:mx-0 self-center', descBlockClass)}>
-        <h2 className="pb-4">{title}</h2>
+        <Link href={href}>
+          <a href={href} title={href}>
+            <h2 className="pb-4 text-black">{title}</h2>
+          </a>
+        </Link>
         <p className="text-xs leading-normal text-justify justify-fix">{description}</p>
-        <Link href="/agaetis">
+        <Link href={href}>
           <Button
-            href="/agaetis"
+            href={href}
             className="px-6 py-2 leading-none rounded-full uppercase mt-4 mb-6 md:mb-0 bg-orange text-white text-xs font-semibold inline-block"
           >
             {buttonContent}
           </Button>
         </Link>
       </div>
-      <img className="home-img mx-auto md:mx-0" src={imgUrl} />
+      <img className="home-img mx-auto md:mx-0" src={imgUrl} alt={imgUrl} />
     </div>
   )
 }
