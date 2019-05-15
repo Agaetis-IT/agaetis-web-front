42aaad6be93b1b6fb00b57f50d6dc91230ddadff
benoit munoz <mun.benoit@gmail.com>
2019-05-15 11:42:21 +0200

    :white_check_mark: Add validation message

--- src/hooks/useForminput.tsx
+++ src/hooks/useForminput.tsx
@@ -2,13 +2,20 @@ import { useCallback, useState } from 'react'
 
 export default function useFormInput(initialValue: any) {
   const [value, setValue] = useState(initialValue)
-
+  const [isValidField, setIsValidField] = useState(true)
   const handleChange = useCallback(e => {
     setValue(e.target.value)
   }, [])
 
+  const handleValidation = (isValid: boolean) => {
+    setIsValidField(isValid)
+    return isValid
+  }
+
   return {
     value,
     onChange: handleChange,
+    handleValidation,
+    isValidField,
   }
 }
