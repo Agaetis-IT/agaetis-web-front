c148b3c7a2cae34da653bfb09910d7131e95eae4
benoit munoz <mun.benoit@gmail.com>
2019-09-19 10:16:19 +0200

    :sparkle: job carousel

--- src/components/JobsCarousel.tsx
+++ src/components/JobsCarousel.tsx
@@ -1,6 +1,10 @@
 import clsx from 'clsx'
 import React, { useState } from 'react'
 
+import arrowL from '../images/left-arrow.svg'
+import Nicolas from '../images/Nicolas.png'
+import arrowR from '../images/right-arrow.svg'
+
 import Button from './Button'
 import './JobsCarousel.css'
 
@@ -29,21 +33,59 @@ export default function JobsCarousel({ slideMax }: Props) {
 
   return (
     <div className="w-full flex flex-row justify-between">
-      <Button onClick={handlePrevSlide}>Prev</Button>
+      <Button onClick={handlePrevSlide}>
+        <img style={{ width: 40, height: 40, color: 'orange' }} className="text-orange" src={arrowL} />
+      </Button>
       <div>
-        <div className={clsx({ hidden: slideIndex !== 0 }, 'bg-grey m-4')}>
-          <p className="text-sm italic p-8">
-            Dein Syria per speciosam interpatet diffusa planitiem. hanc nobilitat Antiochia, mundo cognita civitas, cui
-            non certaverit alia advecticiis ita adfluere copiis et internis, et Laodicia et Apamia itidemque Seleucia
-            iam.
-          </p>
+        <div className={clsx(slideIndex !== 0 ? 'hidden' : 'flex flex-row', 'bg-grey m-4')}>
+          <div className="p-8 leading-normal pr-4">
+            <p className="text-sm italic">
+              Dein Syria per speciosam interpatet diffusa planitiem. hanc nobilitat Antiochia, mundo cognita civitas,
+              cui non certaverit alia advecticiis ita adfluere copiis et internis, et Laodicia et Apamia itidemque
+              Seleucia iam.
+            </p>
+            <p className="text-blue text-xs font-semibold pt-2">Nicolas Roux</p>
+            <p className="text-blue text-xs italic">Co-fondateur Agaetis</p>
+          </div>
+          <img className="p-8 pl-4" src={Nicolas} />
         </div>
-        <div className={clsx({ hidden: slideIndex !== 1 })}>test1</div>
-        <div className={clsx({ hidden: slideIndex !== 2 })}>test2</div>
-        <div className="sliderCount border-orange" />
+        <div className={clsx(slideIndex !== 1 ? 'hidden' : 'flex flex-row', 'bg-grey m-4')}>
+          <div className="p-8 leading-normal pr-4">
+            <p className="text-sm italic">
+              Dein Syria per speciosam interpatet diffusa planitiem. hanc nobilitat Antiochia, mundo cognita civitas,
+              cui non certaverit alia advecticiis ita adfluere copiis et internis, et Laodicia et Apamia itidemque
+              Seleucia iam.
+            </p>
+            <p className="text-blue text-xs font-semibold pt-2">Nicolas Roux</p>
+            <p className="text-blue text-xs italic">Co-fondateur Agaetis</p>
+          </div>
+          <img className="p-8 pl-4" src={Nicolas} />
+        </div>
+        <div className={clsx(slideIndex !== 2 ? 'hidden' : 'flex flex-row', 'bg-grey m-4')}>
+          <div className="p-8 leading-normal pr-4">
+            <p className="text-sm italic">
+              Dein Syria per speciosam interpatet diffusa planitiem. hanc nobilitat Antiochia, mundo cognita civitas,
+              cui non certaverit alia advecticiis ita adfluere copiis et internis, et Laodicia et Apamia itidemque
+              Seleucia iam.
+            </p>
+            <p className="text-blue text-xs font-semibold pt-2">Nicolas Roux</p>
+            <p className="text-blue text-xs italic">Co-fondateur Agaetis</p>
+          </div>
+          <img className="p-8 pl-4" src={Nicolas} />
+        </div>
+        <div className="flex justify-center">
+          {Array.from(Array(slideMax + 1), (_, index) => (
+            <span
+              key={index}
+              className={clsx({ 'bg-orange': slideIndex === index }, 'sliderCount border-orange mx-1')}
+            />
+          ))}
+        </div>
       </div>
 
-      <Button onClick={handleNextSlide}>Next</Button>
+      <Button onClick={handleNextSlide}>
+        <img src={arrowR} style={{ width: 40, height: 40, color: 'orange' }} />
+      </Button>
     </div>
   )
 }
