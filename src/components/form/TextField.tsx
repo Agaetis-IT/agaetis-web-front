8aef8a3c63b631d24d3290d6ee573c37d6bc08ba
benoit munoz <mun.benoit@gmail.com>
2019-09-26 11:16:35 +0200

    whitepaper form

--- src/components/form/TextField.tsx
+++ src/components/form/TextField.tsx
@@ -8,9 +8,10 @@ interface Props {
   placeholder: string
   label: string
   className: string
+  inputClassName?: string
 }
 
-export default function TextField({ name, isInvalid, placeholder, label, className }: Props) {
+export default function TextField({ name, isInvalid, placeholder, label, className, inputClassName }: Props) {
   return (
     <div className={className}>
       <label htmlFor={name} className="text-xs font-semibold">
@@ -20,7 +21,7 @@ export default function TextField({ name, isInvalid, placeholder, label, classNa
         name={name}
         type="text"
         placeholder={isInvalid ? '' : placeholder}
-        className={clsx(isInvalid ? 'bg-red-light ' : '', 'h-8 px-2 my-2 text-xs outline-none')}
+        className={clsx(isInvalid ? 'bg-red-light ' : '', 'h-8 px-2 my-2 text-xs outline-none', inputClassName)}
       />
       <ErrorMessage name={name} component="div" className="text-cgu font-semibold text-red" />
     </div>
