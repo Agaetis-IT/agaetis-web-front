80f052d6a5ed445ba3352e8e64bfd73f0d84f1c3
benoit munoz <mun.benoit@gmail.com>
2019-10-25 14:59:23 +0200

    :sparkles: Add env

--- src/Services/wordpressService.tsx
+++ src/Services/wordpressService.tsx
@@ -1,17 +1,18 @@
 import axios from 'axios'
 
+import publicRuntimeConfig from '../config/env.config'
 import { IdeasPageContent } from '../types/IdeasContent'
 import IndexContent from '../types/IndexContent'
 import JobsContentAPI, { convertJobsContentAPItoContent } from '../types/JobsContent'
 import SolutionsContentAPI, { convertContentAPItoContent } from '../types/SolutionsContent'
 
 export async function getWordpressPage<T>(id: number) {
-  const { data } = await axios.get<T>(`http://localhost/blogAgaetis/wp-json/wp/v2/pages/${id}`)
+  const { data } = await axios.get<T>(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/wp/v2/pages/${id}`)
   return data
 }
 
 export async function getWordpressPageBySlug<T>(slug: string) {
-  const { data } = await axios.get<T>(`http://localhost/blogAgaetis/wp-json/agaetis/api/v1/pages/${slug}`)
+  const { data } = await axios.get<T>(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/agaetis/api/v1/pages/${slug}`)
   return data
 }
 
@@ -41,32 +42,32 @@ export async function getJobContent(slug: string) {
 }
 
 export async function getAllIdeas() {
-  const { data } = await axios.get('http://localhost/blogAgaetis/wp-json/wp/v2/posts?_embed')
+  const { data } = await axios.get(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/wp/v2/posts?_embed`)
   return data
 }
 
 export async function getAllJobs() {
-  const { data } = await axios.get(`http://localhost/blogAgaetis/wp-json/agaetis/api/v1/jobs`)
+  const { data } = await axios.get(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/agaetis/api/v1/jobs`)
   return data
 }
 
 export async function getIdeaById(id: number) {
-  const { data } = await axios.get(`http://localhost/blogAgaetis/wp-json/wp/v2/posts/${id}?_embed`)
+  const { data } = await axios.get(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/wp/v2/posts/${id}?_embed`)
   return data
 }
 
 export async function getIdeaBySlug(slug: string) {
-  const { data } = await axios.get(`http://localhost/blogAgaetis/wp-json/agaetis/api/v1/posts/${slug}`)
+  const { data } = await axios.get(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/agaetis/api/v1/posts/${slug}`)
   return data
 }
 
 export async function getCategories() {
-  const { data } = await axios.get(`http://localhost/blogAgaetis/wp-json/wp/v2/categories/`)
+  const { data } = await axios.get(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/wp/v2/categories/`)
   return data
 }
 
 export async function sendMessage(name: string, mail: string, content: string, date: Date) {
-  await axios.post(`http://localhost/blogAgaetis/wp-json/agaetis/api/v1/send`, {
+  await axios.post(`${publicRuntimeConfig.NEXT_APP_BASE_URL}/wp-json/agaetis/api/v1/send`, {
     author_name: name,
     author_email: mail,
     content,
