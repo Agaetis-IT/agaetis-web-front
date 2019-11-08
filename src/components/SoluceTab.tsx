f6169c1c08f4bd977d5d26b7de1549d3cf1b18c7
benoit munoz <mun.benoit@gmail.com>
2019-11-08 16:59:02 +0100

    :ok_hand: style to function

--- src/components/SoluceTab.tsx
+++ src/components/SoluceTab.tsx
@@ -11,13 +11,31 @@ interface Props {
 }
 
 function getTabsClassNames(tabIndex: number, currentIndex: number) {
-  if ((tabIndex === 0 && currentIndex === 3) || (tabIndex === 3 && currentIndex === 0)) {
-    return 'hidden sm:block w-1/3 text-black bg-grey hover:bg-orange-light'
+  if (tabIndex === 0) {
+    if (currentIndex === 0) {
+      return 'text-white bg-blue w-1/3 sm:w-1/4 py-4'
+    } else if (currentIndex === 3) {
+      return 'hidden sm:block w-1/3 text-black bg-grey hover:bg-orange-light sm:w-1/4 py-4'
+    }
+    return 'text-black bg-grey hover:bg-orange-light w-1/6 sm:w-1/4 whitespace-no-wrap overflow-hidden reverseText'
+  } else if (tabIndex === 1) {
+    if (currentIndex === 1) {
+      return 'text-white bg-blue w-1/3 sm:w-1/4'
+    }
+    return 'text-black bg-grey hover:bg-orange-light w-1/3 sm:w-1/4'
+  } else if (tabIndex === 2) {
+    if (currentIndex === 2) {
+      return 'text-white bg-blue w-1/3 sm:w-1/4'
+    }
+    return 'text-black bg-grey hover:bg-orange-light w-1/3 sm:w-1/4'
+  } else {
+    if (currentIndex === 3) {
+      return 'text-white bg-blue  px-4 w-auto sm:w-1/4'
+    } else if (currentIndex === 0) {
+      return 'hidden sm:block text-black bg-grey hover:bg-orange-light w-auto sm:w-1/4'
+    }
+    return 'text-black bg-grey hover:bg-orange-light w-1/6 sm:w-1/4 whitespace-no-wrap overflow-hidden w-auto sm:w-1/4'
   }
-  return clsx(
-    'text-black bg-grey hover:bg-orange-light w-1/6 sm:w-1/4 whitespace-no-wrap overflow-hidden' +
-      { reverseText: tabIndex === 0 }
-  )
 }
 
 export default function SoluceTab({ tabs }: Props) {
@@ -33,44 +51,16 @@ export default function SoluceTab({ tabs }: Props) {
   return (
     <>
       <div className="flex flex-row w-full mt-8 md:mt-0">
-        <Button
-          className={clsx(
-            currentIndex === 0 ? 'text-white bg-blue w-1/3' : getTabsClassNames(0, currentIndex),
-            'sm:w-1/4 py-4 '
-          )}
-          styleType="tab"
-          onClick={onTabChange(0)}
-        >
+        <Button className={getTabsClassNames(0, currentIndex)} styleType="tab" onClick={onTabChange(0)}>
           {tabs[0].header}
         </Button>
-        <Button
-          className={clsx(
-            currentIndex === 1 ? 'text-white bg-blue' : 'text-black bg-grey hover:bg-orange-light',
-            'w-1/3 sm:w-1/4'
-          )}
-          styleType="tab"
-          onClick={onTabChange(1)}
-        >
+        <Button className={getTabsClassNames(1, currentIndex)} styleType="tab" onClick={onTabChange(1)}>
           {tabs[1].header}
         </Button>
-        <Button
-          className={clsx(
-            currentIndex === 2 ? 'text-white bg-blue' : 'text-black bg-grey hover:bg-orange-light',
-            'w-1/3 sm:w-1/4'
-          )}
-          styleType="tab"
-          onClick={onTabChange(2)}
-        >
+        <Button className={getTabsClassNames(2, currentIndex)} styleType="tab" onClick={onTabChange(2)}>
           {tabs[2].header}
         </Button>
-        <Button
-          className={clsx(
-            currentIndex === 3 ? 'text-white bg-blue w-1/3 px-4' : getTabsClassNames(3, currentIndex),
-            'w-auto sm:w-1/4'
-          )}
-          styleType="tab"
-          onClick={onTabChange(3)}
-        >
+        <Button className={getTabsClassNames(3, currentIndex)} styleType="tab" onClick={onTabChange(3)}>
           {tabs[3].header}
         </Button>
       </div>
