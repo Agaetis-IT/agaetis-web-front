a18d99c780369eebac34127396af31c8fc49a20f
Bastien Humbert <bhumbert@agaetis.fr>
2020-02-13 14:36:03 +0100

    remove bugsnag on dev env

--- src/bugsnag/bugsnag.ts
+++ src/bugsnag/bugsnag.ts
@@ -6,7 +6,18 @@ let bugsnagClient: any
 
 export default function initBugsnag() {
   if (!bugsnagClient) {
-    bugsnagClient = bugsnag('53ba0f9a8bba328258d4d7c8a14f8e52')
+    bugsnagClient = bugsnag({
+      apiKey: '53ba0f9a8bba328258d4d7c8a14f8e52',
+      beforeSend(report: any) {
+        // Disable reporting of errors in dev env
+        if (
+          !process.env.REACT_APP_ENV ||
+          (process.env.REACT_APP_ENV !== 'PROD' && process.env.REACT_APP_ENV !== 'ACC')
+        ) {
+          report.ignore()
+        }
+      },
+    })
     bugsnagClient.use(bugsnagReact, React)
   }
   return
