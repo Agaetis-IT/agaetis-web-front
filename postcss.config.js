8bfb73387e181d8154c16e4182c93c07aaca3633
benoit munoz <mun.benoit@gmail.com>
2020-05-27 09:11:36 +0200

    :lipstick: agaetis content is larger

--- postcss.config.js
+++ postcss.config.js
@@ -13,7 +13,7 @@ module.exports = {
     tailwind('./tailwind.config.js'),
     purgecss({
       content: ['./src/**/*.tsx'],
-      whitelist: ['html', 'body', 'figure', 'loading', 'p', 'a', 'button'],
+      whitelist: ['html', 'body', 'figure', 'loading', 'p', 'a', 'button', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
       extractors: [
         {
           extractor: TailwindExtractor,
