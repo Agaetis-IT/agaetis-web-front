25e26bb5c50d1608898359851735809ae116a94c
benoit munoz <mun.benoit@gmail.com>
2020-06-09 13:56:20 +0200

    :sparkles: Add icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React from 'react'
+import Particles from 'react-particles-js'
 
 interface Props {
   hero: string
@@ -18,6 +20,51 @@ export default function Hero({ hero, valeurs, subtitle }: Props) {
       }}
       className="m-0 hero"
     >
+      <Particles
+        className="hidden sm:block"
+        params={{
+          particles: {
+            number: {
+              value: 3,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              speed: 1,
+              out_mode: 'out',
+            },
+            shape: {
+              type: ['image'],
+              image: [
+                {
+                  src: '/favicon.ico',
+                  height: 20,
+                  width: 23,
+                },
+              ],
+            },
+            color: {
+              value: '#CCC',
+            },
+            size: {
+              value: 30,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 4,
+                size_min: 30,
+                sync: true,
+              },
+            },
+          },
+          retina_detect: false,
+        }}
+      />
       <div className=" flex justify-center">
         <div className="flex flex-col hero-text md:flex-row p-6 md:p-0 md:my-6 py-16 max-w-sm md:max-w-full text-white mx-auto text-justify justify-end">
           <div className="xl:pr-48 " style={{ transform: 'translate(-50 %, -50 %)' }}>
