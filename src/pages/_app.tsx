cc4e049f73cf4f5a9a258b8f5213281b0b7f16c4
Bastien Humbert <bhumbert@agaetis.fr>
2020-06-22 09:46:46 +0200

    added twitter metas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,6 +54,8 @@ export default class MyApp extends App {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="Language" content="fr" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@AgaetisIT" />
             <link rel="shortcut icon" type="image/png" href={Favicon} />
           </Head>
 
