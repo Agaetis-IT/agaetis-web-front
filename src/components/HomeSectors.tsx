6e18423994ba5056465f71dea197f0f826e44e52
benoit munoz <mun.benoit@gmail.com>
2020-08-17 14:19:44 +0200

    :ok_hand: fix bug for white sectors

--- src/components/HomeSectors.tsx
+++ src/components/HomeSectors.tsx
@@ -13,18 +13,20 @@ export default function HomeSectors({ sectors }: Props) {
     <div className="bg-light-grey py-8 md:p-16 xl:px-32">
       <h2 className="text-orange font-semibold text-center md:text-left">Nos secteurs d'activité</h2>
       <div className=" py-12 sectors-list ">
-        {sectors.map((sector) => (
-          <div key={sector.title} className="md:bg-white items md:shadow-md">
-            <img src={sector.image}></img>
-            <div className="bg-white md:bg-none home-sectors-description pb-4">
-              <h3 className="p-4">{sector.title}</h3>
-              <p className="text-xs text-justify leading-normal p-4">{sector.desc}</p>
-              <Button className="block px-6 py-3 leading-none rounded-full uppercase bg-orange text-white mt-4 text-xs font-semibold mx-auto">
-                En savoir plus
-              </Button>
+        {sectors
+          .filter((sector) => sector.title != '' && sector.desc != '' && sector.image != '')
+          .map((sector) => (
+            <div key={sector.title} className="md:bg-white items md:shadow-md">
+              <img src={sector.image}></img>
+              <div className="bg-white md:bg-none home-sectors-description pb-4">
+                <h3 className="p-4">{sector.title}</h3>
+                <p className="text-xs text-justify leading-normal p-4">{sector.desc}</p>
+                <Button className="block px-6 py-3 leading-none rounded-full uppercase bg-orange text-white mt-4 text-xs font-semibold mx-auto">
+                  En savoir plus
+                </Button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   )
