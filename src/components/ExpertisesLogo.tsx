69ea6e572d7a791f7eae7af664c9952139bbd3a0
Benoit <benoit.munoz@agaetis.fr>
2020-08-25 12:58:46 +0200

    :lipstick: Add mobile display for expertises

--- src/components/ExpertisesLogo.tsx
+++ src/components/ExpertisesLogo.tsx
@@ -1,13 +1,83 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface Props {
   image: string
+  logoId: number
+  title: string
+  onClick(e: React.MouseEvent): void
 }
 
-export default function ExpertisesLogo({ image }: Props) {
+function getLogoPosition(humanPos: number[], logoId: number) {
+  if (window && window.innerWidth < 800) {
+    if (logoId === 0) {
+      return { top: `${humanPos[0] + 65}px`, left: `${humanPos[1] - 85}px` }
+    }
+    if (logoId === 1) {
+      return { top: `${humanPos[0] - 35}px`, left: `${humanPos[1] - 55}px` }
+    }
+    if (logoId === 2) {
+      return { top: `${humanPos[0] - 65}px`, left: `${humanPos[1] + 40}px` }
+    }
+    if (logoId === 3) {
+      return { top: `${humanPos[0] - 35}px`, left: `${humanPos[1] + 110}px` }
+    }
+    if (logoId === 4) {
+      return { top: `${humanPos[0] + 65}px`, left: `${humanPos[1] + 140}px` }
+    }
+    return { top: '0', left: '0' }
+  } else {
+    if (logoId === 0) {
+      return { top: `${humanPos[0] + 65}px`, left: `${humanPos[1] - 100}px` }
+    }
+    if (logoId === 1) {
+      return { top: `${humanPos[0] - 45}px`, left: `${humanPos[1] - 65}px` }
+    }
+    if (logoId === 2) {
+      return { top: `${humanPos[0] - 105}px`, left: `${humanPos[1] + 90}px` }
+    }
+    if (logoId === 3) {
+      return { top: `${humanPos[0] - 45}px`, left: `${humanPos[1] + 245}px` }
+    }
+    if (logoId === 4) {
+      return { top: `${humanPos[0] + 65}px`, left: `${humanPos[1] + 280}px` }
+    }
+    return { top: '0', left: '0' }
+  }
+}
+
+function handleHumanPosition(logoId: number) {
+  return () => {
+    const logoStyle = document.getElementById(`logo-${logoId}`).style
+    const humanPos = [
+      document.getElementById('human2').getBoundingClientRect().top - document.body.getBoundingClientRect().top,
+      document.getElementById('human2').getBoundingClientRect().left,
+    ]
+    const logoPosition = getLogoPosition(humanPos, logoId)
+    logoStyle.position = 'absolute'
+
+    logoStyle.top = logoPosition.top
+    logoStyle.left = logoPosition.left
+  }
+}
+
+export default function ExpertisesLogo({ image, logoId, title, onClick }: Props) {
+  useEffect(() => {
+    if (window && document) {
+      handleHumanPosition(logoId)()
+      window.addEventListener('resize', handleHumanPosition(logoId))
+    }
+    return () => {
+      window.removeEventListener('resize', handleHumanPosition(logoId))
+    }
+  }, [logoId])
+
   return (
-    <div>
-      <img src={image}></img>
+    <div id={`logo-${logoId}`} className="" onClick={onClick}>
+      <img src={image} className="block mx-auto"></img>
+      <p
+        className="text-xs text-center font-semibold"
+        dangerouslySetInnerHTML={{ __html: title.split(/[\s]/).join('<br/>') }}
+      ></p>
     </div>
   )
 }
--- src/components/HomeExpertises.tsx
+++ src/components/HomeExpertises.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Trigramme from './Trigramme'
 
@@ -10,10 +10,36 @@ import Trigram2 from '../static/images/trigram2.png'
 import Trigram3 from '../static/images/trigram3.png'
 import Trigram4 from '../static/images/trigram4.png'
 import Trigram5 from '../static/images/trigram5.png'
+import CustomerC from '../static/icons/ux.png'
+import Organisation from '../static/icons/organisation.png'
+import Data from '../static/icons/datascience.png'
+import Dev from '../static/icons/dev.png'
+import Secu from '../static/icons/cyber-secu.png'
 
 import './HomeExpertises.css'
+import ExpertisesLogo from './ExpertisesLogo'
+import Button from './Button'
+
+const icons = [
+  { index: 0, title: 'Customer Centric', image: CustomerC, items: 'UX, Service design' },
+  {
+    index: 1,
+    title: 'Pilotage/ Organisation',
+    image: Organisation,
+    items: 'Exploration, Culture data, Transformation',
+  },
+  { index: 2, title: 'Data', image: Data, items: 'IA, R&D, Simulation, Data quality, Data science' },
+  { index: 3, title: 'Développement', image: Dev, items: 'Cloud, Big data, Craftsmanship, Objets connectés' },
+  { index: 4, title: 'Sécurité/ Réglementaire', image: Secu, items: 'GDPR, Security by design' },
+]
 
 export default function HomeExpertises() {
+  const [selectedExpertise, setSelectedExpertise] = useState(0)
+  const handleExpertiseChange = (expertiseId: number) => {
+    return () => {
+      setSelectedExpertise(expertiseId)
+    }
+  }
   return (
     <>
       <div className="hidden lg:block  md:p-16 xl:px-32">
@@ -22,7 +48,6 @@ export default function HomeExpertises() {
         </h2>
         <div className="md:mt-36 md:mb-16">
           <img src={Human} className="block mx-auto " id="human"></img>
-
           <Trigramme imageUrl={Trigram3} TrigramOrder={0} line="L" lineClassName="line-L-0"></Trigramme>
           <Trigramme imageUrl={Trigram4} TrigramOrder={1} line="L" lineClassName="line-L-1"></Trigramme>
           <Trigramme imageUrl={Trigram5} TrigramOrder={2} line="R" lineClassName="line-R-0"></Trigramme>
@@ -34,7 +59,26 @@ export default function HomeExpertises() {
         <h2 className="text-orange font-semibold text-center md:text-left">
           Nos expertises : les clés de réussite de vos projets technologiques
         </h2>
-        <img src={Human2} className="block mx-auto my-36" id="human2"></img>
+        <img src={Human2} className="block mx-auto mt-36 mb-16" id="human2"></img>
+        {icons.map((icon) => (
+          <ExpertisesLogo
+            image={icon.image}
+            logoId={icon.index}
+            title={icon.title}
+            onClick={handleExpertiseChange(icon.index)}
+            key={icon.title}
+          ></ExpertisesLogo>
+        ))}
+        {icons[selectedExpertise] && (
+          <div className="bg-light-grey p-8 shadow-xl">
+            <img src={icons[selectedExpertise].image} className="block mx-auto"></img>
+            <h3 className="text-center mt-4 mb-8">{icons[selectedExpertise].title}</h3>
+            <p className="text-center text-sm">{icons[selectedExpertise].items.split(',').join(' - ')}</p>
+            <Button className="block mx-auto bg-white rounded-full shadow-md px-4 py-2 text-orange font-semibold text-xs mt-4">
+              En savoir plus
+            </Button>
+          </div>
+        )}
       </div>
     </>
   )
