54dc99395649da1f862ea47fe1b5a036c34fc3c3
Bastien Humbert <bhumbert@agaetis.fr>
2021-01-18 17:12:15 +0100

    fix regex

--- src/service-worker.js
+++ src/service-worker.js
@@ -81,9 +81,9 @@ registerRoute(
   }),
   'GET'
 )
-registerRoute(/(.*wp-json\/wp\/v2\/posts.*)|(.*agaetis\.fr/blog)/i,                  // Blog posts
+registerRoute(/(.*wp-json\/wp\/v2\/posts.*)|(.*agaetis\.fr\/blog)/i,
   new NetworkFirst({
-    cacheName: 'apis',
+    cacheName: 'blog',
     networkTimeoutSeconds: 10,
     plugins: [new ExpirationPlugin({ maxEntries: 2, maxAgeSeconds: 30 * 24 * 3600, purgeOnQuotaError: !0 })]
   }),
