c08788e1134be1385c2e77fc88710ac55ab94ab1
Cedric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-04-08 10:45:20 +0200

    Blog - extract filtering logic from CategoryTab.tsx in blog.tsx

--- src/components/CategoryTab.tsx
+++ src/components/CategoryTab.tsx
@@ -1,17 +1,15 @@
 import clsx from 'clsx'
-import React, { useMemo, useState } from 'react'
+import React from 'react'
 
-import { Category, IdeasDesc } from '../types/IdeasContent'
+import { Category } from '../types/IdeasContent'
 
 import Button from './Button'
-import IdeasCard from './IdeasCard'
 import './CategoryTab.css'
 
 interface Props {
-  ideasC: IdeasDesc[]
   categories: Category[]
-  ideasImg1?: string
-  ideasImg2?: string
+  categoryFilter: string
+  handleFilterChange: (category: string) => void
 }
 
 function slugify(s: string) {
@@ -29,25 +27,6 @@ function slugify(s: string) {
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, '') // Trim - from end of text
-}
-
-function getBgColor(category: string) {
-  if (category === 'Agaetis' || category === 'Evènements') {
-    return 'bg-orange'
-  } else if (category === 'Stratégie SI') {
-    return 'bg-blue'
-  } else if (category === 'Data') {
-    return 'bg-teal'
-  } else if (category === 'Service Design') {
-    return 'bg-light-pink'
-  } else if (category === 'Technologie') {
-    return 'bg-yellow'
-  } else if (category === 'Agilité') {
-    return 'bg-light-purple'
-  } else if (category === 'Business Hacking') {
-    return 'bg-red-lighter'
-  }
-  return 'bg-grey'
 }
 
 function getBorderColor(category: string, selected: boolean) {
@@ -75,70 +54,7 @@ function getBorderColor(category: string, selected: boolean) {
   return clsx('hover:bg-grey grey-border-thin', selected ? 'bg-grey text-white' : 'text-grey')
 }
 
-function createMarkup(content: string) {
-  return { __html: content }
-}
-
-export default function CategoryTab({ ideasC, categories, ideasImg1, ideasImg2 }: Props) {
-  const [categoryFilter, setFilter] = useState('All')
-  const fakeIdea1 = {
-    id: -1,
-    slug: '',
-    title: '',
-    category: '',
-    categories: [''],
-    tags: [],
-    descriptionText: '',
-    date: '',
-    image: ideasImg1,
-  }
-  const fakeIdea2 = {
-    id: -2,
-    slug: '',
-    title: '',
-    category: '',
-    categories: [''],
-    tags: [],
-    descriptionText: '',
-    date: '',
-    image: ideasImg2,
-  }
-
-  const filteredIdeas = useMemo(() => {
-    const ideas = ideasC.filter(
-      (idea) => categoryFilter === 'All' || idea.categories.includes(categoryFilter) || idea.categories.includes('')
-    )
-
-    if (ideasImg1 && !ideas.find((idea: IdeasDesc) => idea.id === -1)) {
-      ideas.splice(0, 0, fakeIdea1)
-    }
-    if (ideasImg2 && !ideas.find((idea: IdeasDesc) => idea.id === -2)) {
-      ideas.splice(7, 0, fakeIdea2)
-    }
-    return ideas.map((idea) => {
-      return (
-        <div key={idea.id} className={clsx(ideas.length > 2 ? 'sm:w-1/3' : 'sm:w-1/2', ' p-2')}>
-          <IdeasCard
-            className={clsx(
-              { 'shadow-xl hidden sm:block': idea.id < 0 },
-              { [getBgColor(idea.categories.filter((category) => !category.includes('_offer-'))[0])]: idea.id > 0 }
-            )}
-            {...idea}
-            categories={idea.categories.filter((category) => !category.includes('_offer-'))}
-          >
-            <p dangerouslySetInnerHTML={createMarkup(idea.descriptionText)} />
-          </IdeasCard>
-        </div>
-      )
-    })
-  }, [categoryFilter, fakeIdea1, fakeIdea2, ideasC])
-
-  function handleFilterChange(category: string) {
-    return () => {
-      setFilter(category)
-    }
-  }
-
+export default function CategoryTab({ categories, categoryFilter, handleFilterChange }: Props) {
   return (
     <div className="flex flex-col justify-center md:max-w-full mx-auto px-2 py-6 md:p-6 xl:px-12">
       <div className="text-xs mx-auto flex flex-row flex-wrap justify-center">
@@ -147,7 +63,7 @@ export default function CategoryTab({ ideasC, categories, ideasImg1, ideasImg2 }
             'uppercase font-semibold my-2 mx-2 p-2 hover:text-white all rounded-full blue-border-thin',
             categoryFilter === 'All' ? 'bg-blue text-white' : 'blue-border-thin text-blue'
           )}
-          onClick={handleFilterChange('All')}
+          onClick={() => handleFilterChange('All')}
         >
           Toutes
         </Button>
@@ -159,14 +75,11 @@ export default function CategoryTab({ ideasC, categories, ideasImg1, ideasImg2 }
               slugify(category.categoryName),
               getBorderColor(category.categoryName, categoryFilter === category.categoryName)
             )}
-            onClick={handleFilterChange(category.categoryName)}
+            onClick={() => handleFilterChange(category.categoryName)}
           >
             {category.categoryName}
           </Button>
         ))}
-      </div>
-      <div className="flex flex-col md:max-w-lg sm:flex-row justify-center flex-wrap mt-2 md:p-8 mx-auto">
-        {filteredIdeas}
       </div>
     </div>
   )
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 import Button from '../components/Button'
 import CategoryTab from '../components/CategoryTab'
@@ -9,6 +9,7 @@ import publicRuntimeConfig from '../config/env.config'
 import { getAllWhitePapers, getCategories, getIdeasPageContent, getIdeasByPage } from '../Services/wordpressService'
 import { Category, IdeasDesc, IdeasPageContent } from '../types/IdeasContent'
 import WhitePaper from '../types/WhitePaper'
+import IdeasCard from '../components/IdeasCard'
 import clsx from 'clsx'
 import ContactSection from '../components/ContactSection'
 import Particles from '../static/images/particles-3.svg'
@@ -33,10 +34,57 @@ function compareIdeasByDate(idea1: IdeasDesc, idea2: IdeasDesc) {
 
 function Ideas({ ideasDescription, whitePapers, categories, content }: Props) {
   const [sortedIdeas, setSortedIdeas] = useState(ideasDescription.sort(compareIdeasByDate))
-  const [filter, setFilter] = useState('')
+  const [searchFilter, setSearchFilter] = useState('')
   const [isOpenenedModal, setOpenModal] = useState(false)
   const [isError, setIsError] = useState(true)
   const [isSubmited, setIsSubmited] = useState(false)
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [categoryFilter, setCategoryFilter] = useState('All')
+  const [loadedPages, setLoadedPages] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fakeIdea1 = {
+    id: -1,
+    slug: '',
+    title: '',
+    category: '',
+    categories: [''],
+    tags: [],
+    descriptionText: '',
+    date: '',
+    image: content.ideasimg1,
+  }
+
+  const fakeIdea2 = {
+    id: -2,
+    slug: '',
+    title: '',
+    category: '',
+    categories: [''],
+    tags: [],
+    descriptionText: '',
+    date: '',
+    image: content.ideasimg2,
+  }
+
+  function getBgColor(category: string) {
+    if (category === 'Agaetis' || category === 'Evènements') {
+      return 'bg-orange'
+    } else if (category === 'Stratégie SI') {
+      return 'bg-blue'
+    } else if (category === 'Data') {
+      return 'bg-teal'
+    } else if (category === 'Service Design') {
+      return 'bg-light-pink'
+    } else if (category === 'Technologie') {
+      return 'bg-yellow'
+    } else if (category === 'Agilité') {
+      return 'bg-light-purple'
+    } else if (category === 'Business Hacking') {
+      return 'bg-red-lighter'
+    }
+    return 'bg-grey'
+  }
 
   function handleOpenModal(error: boolean) {
     setIsError(error)
@@ -57,8 +105,13 @@ function Ideas({ ideasDescription, whitePapers, categories, content }: Props) {
     }
   }
 
-  const [loadedPages, setLoadedPages] = useState(1)
-  const [isLoading, setIsLoading] = useState(false)
+  function handleFilterChange(category: string) {
+    setCategoryFilter(category)
+  }
+
+  function createMarkup(content: string) {
+    return { __html: content }
+  }
 
   async function handleFetchMoreIdeas() {
     setIsLoading(true)
@@ -94,6 +147,39 @@ function Ideas({ ideasDescription, whitePapers, categories, content }: Props) {
     setIsLoading(false)
   }
 
+  const filteredIdeas = useMemo(() => {
+    const ideas = sortedIdeas.filter(
+      (idea) =>
+        !idea.categories.includes('White-paper') &&
+        !idea.categories.includes('Jobs') &&
+        idea.title.toLocaleLowerCase().includes(searchFilter.toLocaleLowerCase()) &&
+        (categoryFilter === 'All' || idea.categories.includes(categoryFilter) || idea.categories.includes(''))
+    )
+
+    if (content.ideasimg1 && !ideas.find((idea: IdeasDesc) => idea.id === -1)) {
+      ideas.splice(0, 0, fakeIdea1)
+    }
+    if (content.ideasimg2 && !ideas.find((idea: IdeasDesc) => idea.id === -2)) {
+      ideas.splice(7, 0, fakeIdea2)
+    }
+    return ideas.map((idea) => {
+      return (
+        <div key={idea.id} className={clsx(ideas.length > 2 ? 'sm:w-1/3' : 'sm:w-1/2', ' p-2')}>
+          <IdeasCard
+            className={clsx(
+              { 'shadow-xl hidden sm:block': idea.id < 0 },
+              { [getBgColor(idea.categories.filter((category) => !category.includes('_offer-'))[0])]: idea.id > 0 }
+            )}
+            {...idea}
+            categories={idea.categories.filter((category) => !category.includes('_offer-'))}
+          >
+            <p dangerouslySetInnerHTML={createMarkup(idea.descriptionText)} />
+          </IdeasCard>
+        </div>
+      )
+    })
+  }, [categoryFilter, fakeIdea1, fakeIdea2])
+
   return (
     <>
       <Head>
@@ -121,24 +207,22 @@ function Ideas({ ideasDescription, whitePapers, categories, content }: Props) {
               </div>
               <SearchInput // To change
                 handleChange={(value: string) => {
-                  setFilter(value)
+                  if (searchTimeout) clearTimeout(searchTimeout)
+                  setSearchTimeout(setTimeout(() => setSearchFilter(value), 500))
                 }}
               ></SearchInput>
             </div>
 
             <CategoryTab
-              ideasC={sortedIdeas.filter(
-                (idea) =>
-                  !idea.categories.includes('White-paper') &&
-                  !idea.categories.includes('Jobs') &&
-                  idea.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
-              )}
               categories={categories.filter(
                 (category) => category.categoryName !== 'Jobs' && category.categoryName !== 'White-paper'
               )}
-              ideasImg1={content.ideasimg1}
-              ideasImg2={content.ideasimg2}
+              categoryFilter={categoryFilter}
+              handleFilterChange={handleFilterChange}
             />
+            <div className="flex flex-col md:max-w-lg sm:flex-row justify-center flex-wrap mt-2 md:p-8 mx-auto">
+              {filteredIdeas}
+            </div>
             <Button
               className={clsx(
                 'flex flex-row justify-center uppercase rounded-full bg-orange text-xss py-2 px-6 text-white font-semibold mx-auto see-more',
