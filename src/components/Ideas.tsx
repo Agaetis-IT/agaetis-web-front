56e995aeaf4901d79d69d8d4da9bd8a71c3aed30
Cedric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-04-12 16:09:58 +0200

    Spinner

--- src/components/Ideas.tsx
+++ src/components/Ideas.tsx
@@ -20,10 +20,10 @@ import ContactMessage from '../components/ContactMessage'
 import SearchInput from '../components/SearchInput'
 import Error from '../pages/_error'
 
-import './Ideas.css'
 import _ from 'lodash'
 import { getBgColor } from '../Services/categoryColor'
 import { createMarkup, slugify } from '../Services/textUtilities'
+import LoadingSpinner from './LoadingSpinner'
 
 interface Props {
   ideasDescription: IdeasDesc[]
@@ -194,52 +194,11 @@ function Ideas({ ideasDescription, whitePapers, categories, content, errorCode, 
               categoryFilter={categoryFilter}
               handleFilterChange={handleFilterChange}
             />
-            {!isLoadingPosts ? (
-              <>
-                <style jsx global>{`
-                  #nprogress {
-                    pointer-events: none;
-                  }
-                  #nprogress .spinner {
-                    display: 'block';
-                    position: fixed;
-                    z-index: 1031;
-                    top: 15px;
-                    right: 15px;
-                  }
-                  #nprogress .spinner-icon {
-                    width: 18px;
-                    height: 18px;
-                    box-sizing: border-box;
-                    border: solid 2px transparent;
-                    border-top-color: currentColor;
-                    border-left-color: currentColor;
-                    border-radius: 50%;
-                    -webkit-animation: nprogresss-spinner 400ms linear infinite;
-                    animation: nprogress-spinner 400ms linear infinite;
-                  }
-                  @-webkit-keyframes nprogress-spinner {
-                    0% {
-                      -webkit-transform: rotate(0deg);
-                    }
-                    100% {
-                      -webkit-transform: rotate(360deg);
-                    }
-                  }
-                  @keyframes nprogress-spinner {
-                    0% {
-                      transform: rotate(0deg);
-                    }
-                    100% {
-                      transform: rotate(360deg);
-                    }
-                  }
-                `}</style>
-                <div className="flex flex-row justify-center spinner" id="nprogress">
-                  <div className="spinner-icon mr-2"></div>
-                  Chargement
-                </div>
-              </>
+            {isLoadingPosts ? (
+              <div className="flex flex-row justify-center">
+                <LoadingSpinner color="#ff7f40" size={18} />
+                Chargement
+              </div>
             ) : (
               <div className="invisible">-</div>
             )}
@@ -255,27 +214,8 @@ function Ideas({ ideasDescription, whitePapers, categories, content, errorCode, 
                 onClick={() => handleFetchIdeas()}
               >
                 {isLoadingPosts ? (
-                  <div>
-                    <svg
-                      className="animate-spin spinner h-07 w-07 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
+                  <div className="flex flex-row justify-center">
+                    <LoadingSpinner color="#ffffff" size={12} />
                     Chargement
                   </div>
                 ) : (
