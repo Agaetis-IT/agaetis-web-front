122ca428394c274ede3ffa6a1a50d6fe68d8519c
Cédric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-05-14 10:06:32 +0200

    Offers desktop

--- src/components/HomeOffers.tsx
+++ src/components/HomeOffers.tsx
@@ -30,15 +30,25 @@ export default function HomeOffers({ title, offers }: Props) {
     <div className="flex flex-col md:flex-row py-8 md:py-0">
       <div className="home-offers-left p-0 md:p-12 lg:px-24 lg:p-16">
         <h2 className="mt-8 mb-4 md:my-0 text-orange font-bold text-center md:text-left">{title}</h2>
-        <ul className="md:my-12">
+        <div className="md:my-12">
           {offers.map((offer, index) => (
-            <li key={index} className="my-0 md:my-6" id={`offer-${index}`}>
+            <div key={index} className="my-0 md:my-6" id={`offer-${index}`}>
               <div className="p-4 md:p-0">
-                <Button onClick={() => handleOfferChange(index)}>
-                  <span>
-                    <h4 className="text-orange text-sm font-bold uppercase text-left">{offer.title}</h4>
-                    <p className="text-xs italic my-1 text-grey md:text-black text-left">{offer.short_desc}</p>
-                  </span>
+                <Button onClick={() => handleOfferChange(index)} className="w-full">
+                  <div className="flex items-center">
+                    <div className="w-9/10">
+                      <h4 className="text-orange text-sm font-bold uppercase text-left">{offer.title}</h4>
+                      <p className="text-xs italic my-1 text-grey md:text-black text-left">{offer.short_desc}</p>
+                    </div>
+                    <span
+                      className={clsx(
+                        'w-1/10',
+                        index === selectedOffer ? 'text-orange font-bold text-xl' : 'text-grey'
+                      )}
+                    >
+                      {'>'}
+                    </span>
+                  </div>
                 </Button>
                 <Button
                   className="block md:hidden text-grey-darker font-bold mx-2"
@@ -100,9 +110,9 @@ export default function HomeOffers({ title, offers }: Props) {
                   </ul>
                 </div>
               </div>
-            </li>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
       <div
         style={{
