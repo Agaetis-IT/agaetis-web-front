44e35a11406371c8049596aa0d8f64279db3b41c
Cédric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-06-29 09:54:00 +0200

    Footer

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,6 +1,13 @@
 import BottomNav from './BottomNav'
 import Address from './Address'
+import Button from './Button'
+
+const Twitter = '/icons/twitter.png'
+const Linkedin = '/icons/linkedin.png'
+const Facebook = '/icons/facebook.png'
 const Logo = '/images/logo-agaetis-vert.png'
+
+import styles from '../styles/Common.module.css'
 
 export default function ContactSection() {
   const addresses = [
@@ -28,21 +35,49 @@ export default function ContactSection() {
   ]
 
   return (
-    <div className="bg-orange-500 py-8 flex flex-col md:flex-row justify-center text-white text-center md:text-left">
-      <img
-        className="max-w-xxs py-4 px-16 md:px-0 md:pr-16 mx-auto md:mx-0 md:mr-8 mb-4 md:my-0 border-white border-solid border-b md:border-b-0 md:border-r flex flex-col justify-center"
-        src={Logo}
-        alt="Logo"
-      />
-      <div className="flex flex-col justify-center">
-        <h4 className="text-white my-4 mb-4 uppercase text-sm leading-normal">Nos adresses</h4>
-        <div className="flex flex-col md:flex-row justify-center font-extralight mb-4">
-          {addresses.map((address) => (
-            <Address key={address.agency} {...address} />
-          ))}
+    <>
+      <div className="bg-orange-500 p-6 md:p-16 lg:px-32 xl:px-48 flex flex-col md:flex-row text-white text-center md:text-left">
+        <img
+          className="self-center object-contain max-w-xxs h-auto py-4 px-16 md:px-0 md:pr-16 md:mx-0 md:mr-8 mb-4 md:my-0 md:border-white md:border-solid md:border-r"
+          src={Logo}
+          alt="Logo"
+        />
+        <div className="flex flex-col justify-center w-full">
+          <BottomNav />
+          <h4 className="text-white my-4 mb-4 uppercase text-sm font-bold leading-normal">Nos adresses</h4>
+          <div className="flex flex-col md:flex-row justify-between font-extralight mb-4">
+            {addresses.map((address, index) => (
+              <>
+                {index > 0 && <div className="h-px md:hidden w-10 bg-white mb-4 self-center"/>}
+                <Address key={address.agency} {...address} />
+              </>
+            ))}
+          </div>
         </div>
-        <BottomNav />
       </div>
-    </div>
+      <div className="bg-gray-800 text-white py-2 px-6 md:px-16 lg:px-32 xl:px-48 flex justify-center md:justify-between">
+        <span className="text-cgu self-center">© {new Date().getFullYear()} Agaetis - Tous droits réservés</span>
+        <div className="hidden flex-row items-center md:flex">
+          <Button
+            href="https://fr-fr.facebook.com/AgaetisIT"
+            className={`w-6 h-6 mr-4 self-center shadow-sm hover:shadow-md bg-gray-800 hover:bg-gray-700 rounded-full ${styles.smoothTransition} p-1 text-none`}
+          >
+            <img src={Facebook} className="w-4 h-4 filter invert" alt="Facebook" />
+          </Button>
+          <Button
+            href="https://www.linkedin.com/company/agaetis/"
+            className={`w-6 h-6 mr-4 shadow-sm hover:shadow-md bg-gray-800 hover:bg-gray-700 rounded-full ${styles.smoothTransition} p-1 text-none`}
+          >
+            <img src={Linkedin} className="w-4 h-4 filter invert" alt="LinkedIn" />
+          </Button>
+          <Button
+            href="https://twitter.com/agaetisit"
+            className={`w-6 h-6 shadow-sm hover:shadow-md bg-gray-800 hover:bg-gray-700 rounded-full ${styles.smoothTransition} p-1 text-none`}
+          >
+            <img src={Twitter} className="w-4 h-4 filter invert" alt="Twitter" />
+          </Button>
+        </div>
+      </div>
+    </>
   )
 }
