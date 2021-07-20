508ca3775e57bce8b8313a0bd5e8034adfa282c5
Cédric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-07-20 12:07:29 +0200

    Loading bar fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react'
 import Bugsnag from '@bugsnag/js'
 import Head from 'next/head'
-
-import LoadingComponent from '../components/LoadingComponent'
+import NextNProgress from 'nextjs-progressbar'
 
 import initBugsnag from '../bugsnag/bugsnag'
 import initReactGA, { trackUrl } from '../analytics/analytics'
@@ -61,7 +60,7 @@ export default function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" type="image/ico" href={`${process.env.NEXT_PUBLIC_SITE_URL}/favicon.ico`} />
         <link rel="preconnect" href="https://wordpress.agaetis.fr" />
       </Head>
-      <LoadingComponent color="#ff7f40" startPosition={0.25} stopDelayMs={50} height={3} />
+      <NextNProgress color="#ff7f40" startPosition={0.25} stopDelayMs={50} height={3} />
       <Component {...pageProps} />
     </ErrorBoundary>
   )
