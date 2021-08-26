343d9c18d07e6db5bba23c9447df830817259c9f
Cédric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-08-26 17:41:04 +0200

    Simpler regexp

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -45,7 +45,7 @@ function formatAuthorList(authors: AuthorLink[]) {
 }
 
 function wrapImages(content: string) {
-  return content.replace(/(?<=<img[^>]*class="[^"]*)"(?=[^>]*>)/g, ' data-zoomable"')
+  return content.replace(/(?<=<img) (?=[^>]*>)/g, ' data-zoomable ')
 }
 
 function getTopOffset() {
@@ -107,7 +107,7 @@ function PostContent({ content, meta }: Props) {
 
   useEffect(() => {
     setLocation(window.location.href)
-    mediumZoom('.data-zoomable', { margin: 25 })
+    mediumZoom('[data-zoomable]', { margin: 25 })
 
     return setAnchorHandlers()
   }, [setAnchorHandlers])
