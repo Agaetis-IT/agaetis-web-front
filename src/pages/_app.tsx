7fe2fcdc54ff76384daebd23c2834885721882a7
Cédric Klodzinski <cedric.klodzinski@agaetis.fr>
2021-08-26 17:22:10 +0200

    Style fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import NextNProgress from 'nextjs-progressbar'
 import initBugsnag from '../bugsnag/bugsnag'
 import initReactGA, { trackUrl } from '../analytics/analytics'
 
-import 'tocbot/src/scss/tocbot.scss'
 import '../styles/globals.css'
 
 declare global {
